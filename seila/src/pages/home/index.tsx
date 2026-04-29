371e51330371d54fe9f2449fe30543053f1650db
luana <turminaluana@gmail.com>
2026-04-29 17:14:47 -0300

    aula don

---
+++ seila/src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.scss'
+
+export default function Home() {
+    return (
+        <div className={style.home}>
+            <h1>Bem vindo ao site</h1>
+        </div>
+    );
+}
